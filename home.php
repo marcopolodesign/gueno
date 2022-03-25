dffd21f21592767eb3089e2c5af4e1491b4248e5
marcopolodesign <hi@marcopoloca.com>
2022-03-25 14:29:23 -0300

    Home Skeleton ok

--- home.php
+++ home.php
@@ -110,13 +110,116 @@
   </section>
 
 
-  <section class="bg-white relative z-5 w-100 pv6">
-    <h1 class="fw4 f1 tc black pt5">AML & KYC Compliance Solution for LatAm</h1>
-    <h2 class="fw3 f3 tc black mt2">Go safe, go massive.</h2>
+  <section class="bg-white relative z-5 w-100 pv6 mp-items-container container relative">
+    <div class="mp-items-inner-c z-5">
+      <h1 class="fw4 f1 tc black pt5 measure center ph6-ns">AML & KYC Compliance Solution for LatAm</h1>
+      <h2 class="fw3 f3 tc black mt2">Go safe, go massive.</h2>
+
+      <div class="mp-items-inner flex column-mobile justify-between items-stretch mt5">
+        <div class="mp-item smooth-t pa3 mh3-ns">
+          <img class="icon db ml-0 mr-auto" src="/wp-content/uploads/2022/03/Screen-Shot-2022-03-18-at-15.27-1.jpg">
+          <h3 class="f4 black mv2">KYC - KNOW YOUR CLIENT</h3>
+          <p>Stop fraud, protect your business and your clients.</p>
+        </div>
+
+        <div class="mp-item smooth-t pa3 mh3-ns">
+          <img class="icon db ml-0 mr-auto" src="/wp-content/uploads/2022/03/Screen-Shot-2022-03-18-at-15.27-1.jpg">
+          <h3 class="f4 black mv2">AML - ANTI-MONEY LAUDERING</h3>
+          <p>No black money in your Ecosystem. Even for crypto funds.</p>
+        </div>
+
+        <div class="mp-item smooth-t pa3 mh3-ns">
+          <img class="icon db ml-0 mr-auto" src="/wp-content/uploads/2022/03/Screen-Shot-2022-03-18-at-15.27-1.jpg">
+          <h3 class="f4 black mv2">COMPLIANCE — LATAM FOCUSED</h3>
+          <p>Stop fraud, protect your business and your clients.</p>
+        </div>
+    </div>
+
+    <div class="absolute top-0 mp-items-bg z--1 flex align-center">
+      <?php get_template_part('template-parts/content/gradient-bg'); ?>
+    </div>
+    </div>
   </section>
+
+
+  <section class="mp-text-image mt5 flex jic column-reverse-mobile container-left ">
+    <div class="w-40-ns">
+      <p class="f6">ONE API. +20 COUNTRIES</p>
+      <h1 class="f1 mt3 mb2">Designed by and for developers</h1>
+      <p class="lh-copy measure-wide">Nos sacrificamos para que sus equipos no necesiten unir sistemas dispares o pasar meses integrando las funcionalidades y pagos.</p>
+      <a class="main-cta flex has-gradient mt4">
+        <p>Read our docs</p>
+      </a>
+    </div>
+
+    <div class="w-50-ns flex bg-main-gradient pv5-ns">
+      <img class="pa5" src="/wp-content/uploads/2022/03/Screen-Shot-2022-03-21-at-13.46-1.png" class="m-auto">
+    </div>
+  </section>
+
+
+  <section class="mp-data-numbers flex jic container mv6">
+      <h1 class="f2 tc">+3M<br>users</h1>
+      <h1 class="f2 tc">+20<br>countries</h1>
+      <h1 class="f2 tc">+200<br>integrations</h1>
+  </section>
+
+  <section class="container mb6">
+    <div class="mp-banner-cta bg-secondary-gradient pa4 gueno-br relative smooth-t">
+      <div class="relative z-2">
+        <h1 class="tc white f1">Ready to begin?<br>Let's talk</h1>
+        <div class="flex justify-center items-center mt3">
+          <a href="#" class="main-cta bg-white no-deco tc m-auto">
+            <p class="main-dark-color tc">Read our docs</p>
+          </a>
+        </div>
+      </div>
+      <div class="absolute-cover bg-main-gradient o-0 smooth-t bg-center no-repeat smooth-t gueno-br"></div>
+
+      <div class="absolute-cover bg-center no-repeat smooth-t">
+        <svg width="100%" height="320" viewBox="0 0 1238 320" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M107.413 237.339L10.4878 140.414V141.828L105.999 237.339H107.413ZM-105.025 237.339L-9.51218 141.825V140.411L-106.44 237.339H-105.025ZM-106.439 257.339L-105.025 257.339L-9.5122 352.851L-9.5122 354.265L-106.439 257.339ZM105.998 257.339H107.412L10.4878 354.263V352.849L105.998 257.339Z" fill="white"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M106.777 2.85273L9.85039 -94.0737V-92.6595L105.363 2.85273H106.777ZM-105.661 2.85274L-10.1496 -92.6586V-94.0728L-107.075 2.85274H-105.661ZM-107.075 22.8527H-105.661L-10.1496 118.364V119.778L-107.075 22.8527ZM105.362 22.8527H106.777L9.85037 119.779V118.365L105.362 22.8527Z" fill="white"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M341.266 3.48823L244.339 -93.4382V-92.024L339.851 3.48823H341.266ZM128.825 3.48824L224.339 -92.026V-93.4402L127.411 3.48824H128.825ZM127.414 23.4882H128.828L224.339 118.999V120.413L127.414 23.4882ZM339.848 23.4882H341.262L244.339 120.412V118.997L339.848 23.4882Z" fill="white"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M1120.85 73.9255L1217.78 170.85V169.436L1122.27 73.9255H1120.85ZM1333.29 73.9255L1237.78 169.439V170.853L1334.7 73.9255H1333.29ZM1334.7 53.9255L1333.29 53.9255L1237.78 -41.587L1237.78 -43.0012L1334.7 53.9255ZM1122.27 53.9255H1120.85L1217.78 -42.9988V-41.5846L1122.27 53.9255Z" fill="white"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M1121.49 308.411L1218.41 405.338V403.924L1122.9 308.411H1121.49ZM1333.93 308.411L1238.41 403.923V405.337L1335.34 308.411H1333.93ZM1335.34 288.411H1333.92L1238.41 192.9V191.486L1335.34 288.411ZM1122.9 288.411H1121.49L1218.41 191.485V192.899L1122.9 288.411Z" fill="white"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M886.999 307.776L983.925 404.702V403.288L888.413 307.776H886.999ZM1099.44 307.776L1003.92 403.29V404.704L1100.85 307.776H1099.44ZM1100.85 287.776H1099.44L1003.92 192.265V190.851L1100.85 287.776ZM888.416 287.776H887.002L983.925 190.853V192.267L888.416 287.776Z" fill="white"/>
+        </svg>
+      </div>
+    </div>
+  </section>
+ 
 </main><!-- #main & End Barba Container-->
 
 <?php get_footer();?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 <style>
---
+++ template-parts/content/gradient-bg.php
@@ -0,0 +1,28 @@
+<svg width="891" height="681" viewBox="0 0 891 681" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_f_96_112)">
+<circle cx="333" cy="335.808" r="300" fill="url(#paint0_radial_96_112)"/>
+</g>
+<g filter="url(#filter1_f_96_112)">
+<circle cx="569" cy="335.808" r="300" transform="rotate(120 569 335.808)" fill="url(#paint1_radial_96_112)"/>
+</g>
+<defs>
+<filter id="filter0_f_96_112" x="-9" y="-6.19235" width="684" height="684" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+<feGaussianBlur stdDeviation="21" result="effect1_foregroundBlur_96_112"/>
+</filter>
+<filter id="filter1_f_96_112" x="226.95" y="-6.24246" width="684.1" height="684.1" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+<feGaussianBlur stdDeviation="21" result="effect1_foregroundBlur_96_112"/>
+</filter>
+<radialGradient id="paint0_radial_96_112" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(128 143.808) rotate(46.2174) scale(582.434 853.078)">
+<stop stop-color="#66CA70"/>
+<stop offset="1" stop-color="#9FD6FE" stop-opacity="0"/>
+</radialGradient>
+<radialGradient id="paint1_radial_96_112" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(364 143.808) rotate(46.2174) scale(582.434 853.078)">
+<stop stop-color="#66CA70"/>
+<stop offset="1" stop-color="#9FD6FE" stop-opacity="0"/>
+</radialGradient>
+</defs>
+</svg>
